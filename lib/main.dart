4f21172e21e3904360a88bad4f725d7c904b55c2
Hasan Shukurov <hasan.shukurof@gmail.com>
2024-07-25 11:00:51 +0400

    First Commit

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:request_api/home_page.dart';
+
+void main(List<String> args) {
+  runApp(RequestApi());
+}
+
+class RequestApi extends StatelessWidget {
+  const RequestApi({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("Wheather App"),
+        ),
+        body: const HomePage(),
+      ),
+    );
+  }
+}
